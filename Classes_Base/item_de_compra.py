79873e01bc5fb7b7b634d3afe34abc13cdeff919
duduzz1928 <146479077+duduzz1928@users.noreply.github.com>
2023-10-01 19:38:04 -0300

    Update item_de_compra.py

--- Classes_Base/item_de_compra.py
+++ Classes_Base/item_de_compra.py
@@ -1,1 +1,16 @@
 # Definicao da classe item de compra, que representa um item do carrinho de compra
+
+from produto import Produto
+
+class Item_de_compra():
+    
+    def __init__(self,produto:Produto, quantidade_comprada:int):
+        if (type(produto) is not Produto) or (type(quantidade_comprada) is not int) or (quantidade_comprada<=0):
+            print("Informacoes invalidas")
+            return None
+        self.produto = produto
+        self.quantidade = quantidade_comprada
+    
+    def Custo(self):
+        return(self.produto.preco()*self.quantidade)
+    
