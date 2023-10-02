a4de7158af53f092c8e8cb7ec2b545dbc11e1dc7
gwpcastro <146481202+gwpcastro@users.noreply.github.com>
2023-10-02 12:19:42 -0300

    Update compra.py

--- Classes_Base/compra.py
+++ Classes_Base/compra.py
@@ -1,3 +1,15 @@
 # Definicao da classe compra, que representa o carrinho de compras
 
+from pessoa import Pessoa
+from produto import Produto
+
+class Compra():
+  
+  def __init__(self, cliente:Pessoa, itens:Produto):
+    
+    if(type(cliente is not Pessoa) or type(itens) is not Produto):
+        print("Informacoes invalidas")
+        return None
+    self.cliente = cliente
+    self.itens = itens
 
