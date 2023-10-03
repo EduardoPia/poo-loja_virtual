73fa9570f1be5ef0a2cfabde160ca83eff0540cb
gwpcastro <146481202+gwpcastro@users.noreply.github.com>
2023-10-03 12:44:05 -0300

    Update produto.py

--- Classes_Base/produto.py
+++ Classes_Base/produto.py
@@ -13,11 +13,11 @@ class Produto:
         
     #incrementa a quantidade em estoque  
     def registrar_aquisicao(self,qtd_comprados):
-      self._qtd_estoque = self._qtd_estoque + self.qtd_comprados
+      self._qtd_estoque = self._qtd_estoque + qtd_comprados
         
     #decrementa a quantidade em estoque  
     def registrar_venda(self,qtd_comprados):
-      self._qtd_estoque = self._qtd_estoque - self.qtd_comprados
+      self._qtd_estoque = self._qtd_estoque - qtd_comprados
         
     #retorna a quantidade em estoque  
     def quantidade_estoque(self):
@@ -25,12 +25,12 @@ class Produto:
 
     #atualiza o desconto, valindando a entrada
     def atualizar_desconto(self, novo_desconto):
-      if(type(self.novo_desconto) is not float or self.novo_desconto<0 and self.novo_desconto>=1):
+      if(type(novo_desconto) is not float or novo_desconto<0 and novo_desconto>=1):
           print("Valores invalidos")
       self._desconto = self.novo_desconto
 
     #atualiza preco, validando a entrada
     def atualizar_preco(self, novo_preco):
-      if(type(self.novo_preco) is not float or self.novo_preco<=0):
+      if(type(novo_preco) is not float or novo_preco<=0):
           print("Valores invalidos")
       self._preco = self.novo_preco
