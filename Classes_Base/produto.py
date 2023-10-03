3b8b8b06409e4e3379764e3bd5eb617c53ef84d7
gwpcastro <146481202+gwpcastro@users.noreply.github.com>
2023-10-03 12:21:02 -0300

    Update produto.py

--- Classes_Base/produto.py
+++ Classes_Base/produto.py
@@ -1,3 +1,27 @@
 # Definicao da classe produto
 class Produto:
+    def __init__(self, nome:str, preco:float, categoria:str):
+      sel.nome = nome
+      self._preco = 0
+      self.categoria = categoria
+      self._qtd_estoque = 0
+      self._desconto = 0.0
   
+    #retorna preco com desconto  
+    def preco(self): 
+      return self._preco*self._desconto
+      
+    def registrar_aquisicao(self,qtd_comprados):
+      self._qtd_estoque = self._qtd_estoque + self.qtd_comprados
+      
+    def registrar_venda(self,qtd_comprados):
+      self._qtd_estoque = self._qtd_estoque - self.qtd_comprados
+      
+    def quantidade_estoque(self):
+      return self._qtd_estoque
+  
+    def atualizar_desconto(self, novo_desconto):
+      if(...):
+
+    def atualizar_preco(self, novo_preco):
+      if(...):
