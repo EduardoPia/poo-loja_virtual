d684cf16e02da21e64493bf50647636960c68f80
Eduardo Piassaroli de Abreu <146479077+EduardoPia@users.noreply.github.com>
2023-10-06 12:34:51 -0300

    Update produto.py

--- Classes_Base/produto.py
+++ Classes_Base/produto.py
@@ -2,35 +2,35 @@
 class Produto:
     def __init__(self, nome:str, preco:float, categoria:str):
       self.nome = nome
-      self._preco = 0
+      self.__preco = 0
       self.categoria = categoria
-      self._qtd_estoque = 0
-      self._desconto = 0.0
+      self.__qtd_estoque = 0
+      self.__desconto = 0.0
   
     #retorna preco com desconto  
     def preco(self): 
-      return self._preco*(1-self._desconto)
+      return self.__preco*(1-self._desconto)
         
     #incrementa a quantidade em estoque  
     def registrar_aquisicao(self,qtd_comprados):
-      self._qtd_estoque = self._qtd_estoque + qtd_comprados
+      self.__qtd_estoque = self.__qtd_estoque + qtd_comprados
         
     #decrementa a quantidade em estoque  
     def registrar_venda(self,qtd_comprados):
-      self._qtd_estoque = self._qtd_estoque - qtd_comprados
+      self.__qtd_estoque = self.__qtd_estoque - qtd_comprados
         
     #retorna a quantidade em estoque  
     def quantidade_estoque(self):
-      return self._qtd_estoque
+      return self.__qtd_estoque
 
     #atualiza o desconto, valindando a entrada
     def atualizar_desconto(self, novo_desconto):
       if(type(novo_desconto) is not float or novo_desconto<0 and novo_desconto>=1):
           print("Valores invalidos")
-      self._desconto = self.novo_desconto
+      self.__desconto = self.novo_desconto
 
     #atualiza preco, validando a entrada
     def atualizar_preco(self, novo_preco):
       if(type(novo_preco) is not float or novo_preco<=0):
           print("Valores invalidos")
-      self._preco = novo_preco
+      self.__preco = novo_preco
